54f1a79342851b6e08c198bce50c2be4cc2376c8
JP Quicksall <john.paul.quicksall@gmail.com>
2015-12-01 14:43:52 -0800

    added the trailing slash

--- mk_extension/lib/ruby/facter/guestid.rb
+++ mk_extension/lib/ruby/facter/guestid.rb
@@ -3,6 +3,6 @@ require 'facter'
 Facter.add('guestid') do
   setcode do
     uuid = Facter.value(:uuid)
-    Facter::Core::Execution.exec('curl labapi.bellevuelab.isus.emc.com/vms/#{uuid}/guestid')
+    Facter::Core::Execution.exec('curl labapi.bellevuelab.isus.emc.com/vms/#{uuid}/guestid/')
   end
 end
