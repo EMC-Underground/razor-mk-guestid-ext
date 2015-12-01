46a8c0cd612b487f5d32c52c345c12e5be7d7fe5
JP Quicksall <john.paul.quicksall@gmail.com>
2015-12-01 15:18:55 -0800

    Tweaked the facter value call and the exec quotes

--- mk_extension/lib/ruby/facter/guestid.rb
+++ mk_extension/lib/ruby/facter/guestid.rb
@@ -2,7 +2,7 @@ require 'facter'
 
 Facter.add('guestid') do
   setcode do
-    uuid = Facter.value(:uuid)
-    Facter::Core::Execution.exec('/bin/curl labapi.bellevuelab.isus.emc.com/vms/#{uuid}/guestid/')
+    uuid = Facter.value('uuid')
+    Facter::Core::Execution.exec("/bin/curl labapi.bellevuelab.isus.emc.com/vms/#{uuid}/guestid/")
   end
 end
